acb7b856c87b80946440c9f3539994a3413b429f
Stefan Novak <stefan@novak.io>
2016-08-07 14:36:31 -0700

    whoopse

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate todo;
 use todo::{Todo};
 
 fn main() {
-    let todo = try!(Todo::from_json("{\"description\":\"Hello world\"}"));
-    println!("Description: {}", todo.description);
+    let todo = Todo::from_json("{\"description\":\"Hello world\"}");
+
+    match todo {
+        Ok(todo) => println!("Description: {}", todo.description),
+        Err(err) => println!("Unable to parse!"),
+    }
 }
